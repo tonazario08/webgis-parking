5dbaec9d686301a87560691792e48cbd90cb0462
tonazario <tquoctoan082@gmail.com>
2026-04-21 12:40:37 +0700

    fix: serve media files regardless of DEBUG using django.views.static.serve

--- webgis_parking/urls.py
+++ webgis_parking/urls.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.generic import RedirectView
+from django.views.static import serve
 
 handler404 = "parking.views.custom_page_not_found"
 
@@ -11,7 +12,5 @@ urlpatterns = [
     path("dj-admin/", admin.site.urls),
     path(settings.MANAGER_URL_PREFIX, include("parking.manager_urls")),
     path("", include("parking.urls")),
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
